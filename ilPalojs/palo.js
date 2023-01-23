e9d09f30e3aa413ae88446314316c2cbe302fc3b
gioza <giozanone@gmail.com>
2023-01-24 00:40:59 +0100

    removed unneccessaries packages and bug that banned me from stackoverflaw

--- ilPalojs/palo.js
+++ ilPalojs/palo.js
@@ -1,31 +1,16 @@
 const request = require("request");
 const opn = require("opn");
-const puppeteer = require("puppeteer");
 
 // Define the IP address and port of the Arduino inside the url
 const options = {
-  url: "http://yourIP:80/motion",
+  url: "http://192.168.1.13:80/motion",
   method: "GET",
 };
 const url = "https://stackoverflow.com/";
 var tabExist = false;
 
 setInterval(() => {
-  (async () => {
-    const browser = await puppeteer.launch();
-    const pages = await browser.pages();
-    tabExist = false;
-
-    for (const page of pages) {
-      if (page.url() === "https://stackoverflow.com/") {
-        tabExist = true;
-        break;
-      }
-    }
-    console.log("TabExist: " + tabExist);
-    await browser.close();
-  })();
-
+  
   request(options, (error, response, body) => {
     if (!error && response.statusCode == 200) {
       const data = body.trim();
